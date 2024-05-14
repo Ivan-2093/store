aa4dbffce6c5c1b3211a429701b0f5966cbf0a12
Ivan-2093 <sergioivangalvisesteban@gmail.com>
2024-05-14 17:36:35 -0500

    Second commit

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -9,27 +9,34 @@ import { Component, Input, SimpleChanges } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
-  @Input({required: true}) duration = 0;
-  @Input({required: true}) message = '';
+  @Input({ required: true }) duration = 0;
+  @Input({ required: true }) message = '';
 
-  constructor()
-  {
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor() {
     //No ASYNC
     // Before render
     console.log('Coonstructor');
     console.log('-'.repeat(10));
-    
+
   }
 
-  ngOnChanges(changes: SimpleChanges)
-  {
+  ngOnChanges(changes: SimpleChanges) {
     // Before and during render
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+    
+    if(duration && duration.currentValue != duration.previousValue) {
+      this.doSomething();
+    }
+
   }
 
-  ngOnInit(){
+  ngOnInit() {
     // after render
     // Una unica vez
     // Async, then, subscribe
@@ -37,7 +44,34 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log('duration =>', this.duration);
     console.log('mesagge =>', this.message);
-    
+    this.counterRef = window.setInterval(() => {
+      console.log("Run SetInterval");
+      this.counter.update(statePrev => statePrev + 1);
+    },1000)
+
+  }
+
+  ngAfterViewInit() {
+    //After render!
+    //Hijos ya fueron renderizados!
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+
+  ngOnDestroy() {
+    //After render!
+    //Hijos ya fueron renderizados!
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething()
+  {
+    //async
+    console.log('doSomething 10');
+    console.log('-'.repeat(10));
   }
 
 }
